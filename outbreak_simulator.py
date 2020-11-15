5e5a5b7568864937c30b52911373e4ddd0448071
Abdulla Al Mansoori <abdulla.almansoori@nyu.edu>
2020-11-15 11:45:09 +0400

    added testing rounds of testing subsets

--- outbreak_simulator.py
+++ outbreak_simulator.py
@@ -5,14 +5,47 @@ import matplotlib.pyplot as plt
 import networkx as nx 
 
 
-def update_dynamics(G, contact_rate, removal_rate, dt=1):
+class TestingPool:
+	def __init__(self, tested_nodes, num_chunks=1, schedule=[True]):
+
+		# Initialize a chunk / partition generator
+		self.tested_chunks = self._chunk_generator(tested_nodes, num_chunks)
+
+		# Default test schedule is everyday
+		# Assuming `current_time` == 1 is the first weekday (i.e. Monday)
+		# Example of a schedule where we test everyday except weekends:
+		#     [True, True, True, True, True, False, False]
+		self.current_time = 1
+		self.schedule = schedule
+
+	def _chunk_generator(self, tested_nodes, num_chunks):
+		"""
+		Generates a cycle of `num_chunks` chunks from `tested_nodes`.
+		"""
+		chunk_len = int(np.ceil(len(tested_nodes) / num_chunks))
+		while True:
+			for i in range(0, len(tested_nodes), chunk_len):
+				yield tested_nodes[i:i+chunk_len]
+
+	def next_round(self):
+		"""
+		Get next round of testing subjects.
+		"""
+		testing_round = []
+		if self.schedule[ (self.current_time - 1) % len(self.schedule) ]:
+			testing_round = next(self.tested_chunks)
+		self.current_time += 1
+		return testing_round
+
+
+def update_dynamics(G, infection_rate, recovery_rate, dt=1):
 	"""
 	Updates the dynamics of the graph G (w/o updating the state of the graph).
 
 	Args:
 		G: graph modeling the community
-		contact_rate: contact rate
-		removal_rate: recovery/removal rate
+		infection_rate: contact rate
+		recovery_rate: recovery/removal rate
 		dt: time step
 
 	Returns:
@@ -32,20 +65,20 @@ def update_dynamics(G, contact_rate, removal_rate, dt=1):
 				   and not G.nodes[n]["quarantined"]
 			]
 			# If one or more transmissions happen, the node gets infected
-			p_infection = 1 - (1 - dt*contact_rate) ** len(adj_infected)
+			p_infection = 1 - (1 - dt*infection_rate) ** len(adj_infected)
 			infected = 1 == np.random.binomial(1, p_infection)
 			if infected:
 				node_data["next_state"] = "I"
 
 		# Infected -> Removed
 		if node_data["state"] == "I":
-			if node_data["duration"] >= 1 / removal_rate:
+			if node_data["duration"] >= 1 / recovery_rate:
 				node_data["next_state"] = "R"
 
 	return G
 
 
-def update_tests(G, tested_nodes, current_time, resample=False):
+def update_tests(G, tested_nodes, current_time):
 	"""
 	Update tests (retest testing subjects).
 
@@ -56,11 +89,8 @@ def update_tests(G, tested_nodes, current_time, resample=False):
 		resample: resample test subjects randomly every testing round.
 
 	Returns:
-		Updated graph, and a flag indicating if a test subject was infected.
+		Updated graph, and a flag whether a test subject was infected.
 	"""
-
-	if resample:
-		tested_nodes = np.random.choice(G.nodes, len(tested_nodes))
 
 	tested_positive = False
 	for test_node in tested_nodes:
@@ -107,32 +137,34 @@ def update_state(G, SIR, current_time, quarantine_length):
 def outbreak_simulation(G,
 					    dt=1,
 					    initial_infected=1,
-					    contact_rate=0.1,
-					    removal_rate=0.2,
+					    infection_rate=0.1,
+					    recovery_rate=0.2,
 					    testing_capacity=0.01,
+					    testing_rounds=1,
 					    testing_interval=1,
-					    testing_strategy="random",
-					    quarantine_length=0,
+					    quarantine_length=14,
 					    report_interval=1000,
 					    testing_mode=False,
-					    reproduce=True):
+					    reproduce=False):
 	"""
 	Simulates the spread of an infectious disease in a community modeled
 	by the graph G.
+
+	TODO: There is a bug where the number of infections becomes
+	      very large in magnitude (could be an overflow bug?)
 
 	Args:
 		G: graph modeling the community.
 		dt: time step.
 		initial_infected: number of infected nodes in the beginning.
-		contact_rate: transmission rate (in units of 1/dt).
-		removal_rate: recovery/removal rate (in units of 1/dt).
+		infection_rate: infection rate (in units of 1/dt).
+		recovery_rate: recovery/removal rate (in units of 1/dt).
 		testing_capacity: max tests possible every `testing_interval`,
 		                  measured as a fraction of the whole population.
 		testing_interval: testing interval (in units of dt).
-		testing_strategy: how to sample test subjects from population.
 		quarantine_length: time should be spent in quarantine.
-		report_interval: reporting interval for SIR values (in units of dt).
-		testing_mode: stops simulation when disease reaches a test subject.
+		report_interval: reporting interval for SIR values (in dt).
+		testing_mode: stops simulation when a test subject gets infected.
 
 	Returns:
 		tuple containing S, I, R values of all time steps.
@@ -153,12 +185,11 @@ def outbreak_simulation(G,
 		# Infect some nodes randomly from population
 		infected_nodes = np.random.choice(G.nodes, initial_infected)
 		# Sample test subjects from population
-		num_tests = round(testing_capacity * len(G.nodes))
-		if testing_strategy == "random":
-			tested_nodes = np.random.choice(G.nodes, num_tests)
-		else:
-			raise NotImplementedError(
-				f"Testing strategy '{testing_strategy}' not implemented.")
+		num_tested_nodes = round(testing_capacity * len(G.nodes))
+		tested_nodes = np.random.choice(G.nodes, num_tested_nodes)
+
+	# Create testing pool
+	testing_pool = TestingPool(tested_nodes, num_chunks=testing_rounds)
 
 
 	# Track the number of nodes in each compartment
@@ -195,12 +226,13 @@ def outbreak_simulation(G,
 	while not should_stop():
 
 		# Update dynamics of network
-		G = update_dynamics(G, contact_rate, removal_rate, dt)
+		G = update_dynamics(G, infection_rate, recovery_rate, dt)
 		current_time += dt
 
 		# Retest every testing_interval
 		if round(current_time / dt) % testing_interval == 0:
-			G, tested_positive = update_tests(G, tested_nodes, current_time)
+			testing_round = testing_pool.next_round()
+			G, tested_positive = update_tests(G, testing_round, current_time)
 
 		# Update state of network
 		G, SIR = update_state(G, SIR, current_time, quarantine_length)
@@ -209,12 +241,12 @@ def outbreak_simulation(G,
 		# Show last SIR values
 		if round(current_time / dt) % report_interval == 0:
 			S, I, R = SIR_record[-1]
-			print(f"current_time = {current_time}, S = {S}, I = {I}, R = {R}")
+			print(f"time = {current_time}, S = {S}, I = {I}, R = {R}")
 
 	return tuple(zip(*SIR_record))
 
 
-def plot_infections(num_lines=5, max_t=30):
+def plot_infections(num_lines=20, max_t=30):
 	"""
 	Plot the average SIR curves of `num_lines` curves,
 	and show each infection curve on the plot too.
@@ -224,9 +256,12 @@ def plot_infections(num_lines=5, max_t=30):
 		max_t: plot curves up to `max_t` days.
 	"""
 	N = 4000
-	Q = 40
-	P = 100000
 	G = nx.barabasi_albert_graph(N, 3)
+
+	Q = 400  # num of test subjects
+	testing_capacity = Q / N
+	testing_rounds = 10
+	testing_interval = 1
 
 	lines_shape = (num_lines, max_t+1)
 	S_lines = np.empty(lines_shape)
@@ -234,7 +269,9 @@ def plot_infections(num_lines=5, max_t=30):
 	R_lines = np.empty(lines_shape)
 
 	for i in range(num_lines):
-		S, I, R = outbreak_simulation(G, testing_interval=P)
+		S, I, R = outbreak_simulation(G, testing_capacity=testing_capacity,
+										 testing_rounds=testing_rounds,
+										 testing_interval=testing_interval,)
 		S, I, R = np.array(S), np.array(I), np.array(R)
 		S_lines[i, :S.shape[0]] = S[:max_t+1]
 		I_lines[i, :I.shape[0]] = I[:max_t+1]
@@ -263,15 +300,12 @@ def main():
 	# Define network structure and outbreak configurations
 	G = nx.barabasi_albert_graph(4000, 3)
 	outbreak_config = {
-		"initial_infected": 1,
-		"contact_rate": 0.1,
-		"removal_rate": 0.2,
+		"infection_rate": 0.1,
+		"recovery_rate": 0.2,
 		"testing_capacity": 0.01,
-		"testing_interval": 1,
-		"testing_strategy": "random",
 		"quarantine_length": 0,
 		"report_interval": 1,
-		"testing_mode": False,
+		"testing_mode": True,
 	}
 	# Run the simulation
 	S, I, R = outbreak_simulation(G, **outbreak_config)
